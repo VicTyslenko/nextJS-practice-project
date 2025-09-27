6ee20dd91503be796295a08bebc98449379da6fd
Victor <vidrums86@gmail.com>
2025-09-27 18:13:44 +0100

    feat: added validation to form

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,54 @@
-export default function Page() {
-  return <p>Invoices</p>;
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { fetchInvoicesPages } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+type Props = {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+};
+
+export default async function Page(props: Props) {
+  const searchParams = await props.searchParams;
+
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const query = searchParams?.query || "";
+
+  const totalPages = await fetchInvoicesPages(query);
+  // const currentDate = new Date().toISOString().split("T")[0].split("-");
+  // console.log(currentDate);
+  // const reverseData = [];
+
+  // for (let i = currentDate.length - 1; i >= 0; i--) {
+  //   reverseData.push(currentDate[i]);
+  // }
+
+  // console.log(reverseData);
+
+  const s = "racecarannakayak";
+
+
+  
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">{<Pagination totalPages={totalPages} />}</div>
+    </div>
+  );
 }
